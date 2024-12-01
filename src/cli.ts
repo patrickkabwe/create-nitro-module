ec1cac670d101aa71b2138571b0eb7a66131d4ed
Patrick Kabwe <patrickckabwe@gmail.com>
2024-12-01 13:58:35 +0200

    chore: add create module command

---
+++ src/cli.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import packageJson from "../package.json" assert { type: "json" };
+import { createModule, generateModule } from "./create.js";
+
+const program = new Command();
+program
+  .name("create-nitro-modules")
+  .description(packageJson.description)
+  .version("Create nitro version: 0.1.0", "-v, --version")
+  .option(
+    "-p, --platform <type>",
+    "specify type of platform can be ios or android or both"
+  )
+  .option(
+    "-d, --directory <string>",
+    "specify the directory you would like put the package"
+  )
+  .option("-n, --name <string>", "specify the name of nitro module")
+  .action(createModule);
+
+program
+  .command("generate <name>")
+  .description("generate a hybrid object into the package directory")
+  .action(generateModule);
+
+program.parse();
