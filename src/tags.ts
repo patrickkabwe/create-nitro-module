076204e835496cb5bcb3eb6d459bb7c45b8ff86f
Patrick Kabwe <patrickckabwe@gmail.com>
2024-12-01 18:25:41 +0200

    chore: exclude assets from package js

--- src/tags.ts
+++
@@ -1,4 +0,0 @@
-export const ANDROID_NAME_SPACE_TAG = "<<androidNamespace>>";
-export const ANDROID_CXX_LIB_NAME_TAG = "<<androidCxxLibName>>";
-export const CXX_NAME_SPACE_TAG = "<<cxxNamespace>>";
-export const IOS_MODULE_NAME_TAG = "<<iosModuleName>>";
