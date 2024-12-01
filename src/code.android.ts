80c53845539702a5032be7ea617955499467cd33
Patrick Kabwe <patrickckabwe@gmail.com>
2024-12-01 14:00:28 +0200

    chore: adds native code snippets

---
+++ src/code.android.ts
@@ -0,0 +1,23 @@
+import { toPascalCase } from "./utils.js";
+
+export const androidManifestCode = `
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+</manifest>
+`;
+
+export const getKotlinCode = (moduleName: string, packageT: string) => `
+package ${packageT}
+
+import com.margelo.nitro.${moduleName.toLowerCase()}.Hybrid${toPascalCase(
+  moduleName
+)}Spec
+
+class ${toPascalCase(moduleName)}: Hybrid${toPascalCase(moduleName)}Spec() {
+    override val memorySize: Long
+        get() = 5
+    
+    override fun sumNum(num1: Double, num2: Double): Double {
+        return num1 + num2
+    }
+}
+`;
---
+++ src/code.ios.ts
@@ -0,0 +1,14 @@
+import { toPascalCase } from "./utils.js";
+
+export const getSwiftCode = (moduleName: string) =>
+`import Foundation
+
+class ${toPascalCase(moduleName)}: Hybrid${toPascalCase(moduleName)}Spec {
+    var hybridContext = margelo.nitro.HybridContext()
+    var memorySize: Int { getSizeOf(self) }
+
+    func sumNum(num1: Double, num2: Double) throws -> Double{
+        return num1 + num2
+    }
+}
+`;
