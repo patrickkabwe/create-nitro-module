8b10ee679db5bfa12f724fe879c1aed151d708a8
Patrick Kabwe <patrickckabwe@gmail.com>
2024-12-22 14:51:56 +0200

    feat: remove hybridContext and memorySize from swift example 😍

--- src/code.ios.ts
+++ src/code.ios.ts
@@ -4,9 +4,6 @@ export const getSwiftCode = (moduleName: string) =>
   `import Foundation
 
 class ${toPascalCase(moduleName)}: Hybrid${toPascalCase(moduleName)}Spec {
-    var hybridContext = margelo.nitro.HybridContext()
-    var memorySize: Int { getSizeOf(self) }
-
     func sumNum(num1: Double, num2: Double) throws -> Double{
         return num1 + num2
     }
