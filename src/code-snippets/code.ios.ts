cd3cdff954a94e9b7b63bf32c7376fa099af7e8d
Patrick Kabwe <patrickckabwe@gmail.com>
2025-01-06 05:28:16 +0200

    chore: clean up

--- src/code-snippets/code.ios.ts
+++ src/code-snippets/code.ios.ts
@@ -4,7 +4,7 @@ export const getSwiftCode = (moduleName: string, funcName: string) =>
     `import Foundation
 
 class Hybrid${toPascalCase(moduleName)}: Hybrid${toPascalCase(moduleName)}Spec {
-    func ${funcName}(num1: Double, num2: Double) throws -> Double{
+    func ${funcName}(num1: Double, num2: Double) throws -> Double {
         return num1 + num2
     }
 }
