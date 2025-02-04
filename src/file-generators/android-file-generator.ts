38068dc50a692c782efa72ce577413dd69c4266d
Patrick Kabwe <patrickckabwe@gmail.com>
2025-02-04 09:58:00 +0200

    feat: upgrade nitro v0.22.0

--- src/file-generators/android-file-generator.ts
+++ src/file-generators/android-file-generator.ts
@@ -175,7 +175,7 @@ export class AndroidFileGenerator implements FileGenerator {
         )
 
         const replacements = {
-            [`com.${ANDROID_NAME_SPACE_TAG}`]: this.androidPackageName,
+            [`${ANDROID_NAME_SPACE_TAG}`]: replaceHyphen(config.moduleName),
             [`${ANDROID_CXX_LIB_NAME_TAG}Package`]:
                 androidPackageFile.split('.')[0],
             [ANDROID_CXX_LIB_NAME_TAG]: toPascalCase(config.moduleName),
