ee67738d8cd19b965b9e8a1ead0608dc386ca7cc
Patrick Kabwe <patrickckabwe@gmail.com>
2026-09-19 00:04:40 +0200

    Revert "chore: clean up kotlin snippet"
    This reverts commit b59310e9cee940235da435d242480143da585ed4.

--- src/code-snippets/code.kotlin.ts
+++ src/code-snippets/code.kotlin.ts
@@ -49,13 +49,18 @@ import com.margelo.nitro.${replaceHyphen(moduleName)}.Hybrid${toPascalCase(
 @DoNotStrip
 class Hybrid${toPascalCase(moduleName)}(val context: ThemedReactContext): Hybrid${toPascalCase(moduleName)}Spec() {
     // View
-    override val view = View(context)
+    override val view: View = View(context)
 
     // Props
-    override var isRed: Boolean = false
+    private var _isRed = false
+    override var isRed: Boolean
+        get() = _isRed
         set(value) {
-            field = value
-            view.setBackgroundColor(if (value) Color.RED else Color.BLACK)
+            _isRed = value
+            view.setBackgroundColor(
+                if (value) Color.RED
+                else Color.BLACK
+            )
         }
 }
 `
